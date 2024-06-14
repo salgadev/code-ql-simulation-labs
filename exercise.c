6d93e30316084d26299290f9d6b9d9bf7af25f8e
Carlos Salgado <csalgado@uwo.ca>
2024-06-14 18:52:57 -0400

    check if input is a file

--- exercise.c
+++ exercise.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
                                 
-#define BUFSIZE 256
-                                
+#define BUFSIZE 256                           
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -11,6 +12,14 @@ int main(int argc, char** argv) {
       fprintf(stderr, "Please provide the address of a file as an input.\n");
       return -1;
     }
+    
+    struct stat stbuf;    
+    // Stat the target to examine its properties
+    if (stat(argv[1], &stbuf) == -1) {
+        perror("Failed to obtain information about the entry.");
+        return EXIT_FAILURE;
+    }
+
     char cmd[BUFSIZE] = "wc -c < ";
     strcat(cmd, argv[1]);
     system(cmd);
