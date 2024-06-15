6aca8bad8ba696943aa597442cebd395a910715d
Carlos Salgado <csalgado@uwo.ca>
2024-06-15 14:24:18 -0400

    escape special characters

--- exercise.c
+++ exercise.c
@@ -3,7 +3,17 @@
 #include <string.h>
 #include <sys/stat.h>
                                 
-#define BUFSIZE 256                           
+#define BUFSIZE 256
+
+// This function escapes characters that pose a security risk as whitespace
+void escape_special_chars(char *path) {
+    while (*path) {
+        if (*path == '&' || *path == '|' || *path == '<' || *path == '>' || *path == '(' || *path == ')' || *path == '{' || *path == '}' || *path == ':') {
+            *path = ' ';
+        }
+        path++;
+    }
+}
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
@@ -12,13 +22,8 @@ int main(int argc, char** argv) {
       fprintf(stderr, "Please provide the address of a file as an input.\n");
       return -1;
     }
-    
-    struct stat stbuf;    
-    // Stat the target to examine its properties
-    if (stat(argv[1], &stbuf) == -1) {
-        perror("Failed to obtain information about the entry.");
-        return EXIT_FAILURE;
-    }
+
+    escape_special_chars(argv[1]);
     char cmd[BUFSIZE] = "wc -c < ";    
 
     // unsigned integer for objects size    
