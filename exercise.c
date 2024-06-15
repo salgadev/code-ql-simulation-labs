7648e98967b9166cc98802bade01df633680fd00
Carlos Salgado <csalgado@uwo.ca>
2024-06-15 17:25:28 -0400

    sanitize final command

--- exercise.c
+++ exercise.c
@@ -4,9 +4,8 @@
 #include <sys/stat.h>
 
 #define BUFSIZE 256
-#define ALLOWED_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/_-."
+#define ALLOWED_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/_-.< >|"
 
-// Whitelist function to sanitize file path
 void sanitize_path(char *path) {
     char *p = path;
     while (*p) {
@@ -17,33 +16,25 @@ void sanitize_path(char *path) {
     }
 }
 
-// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
     if (argc != 2) {
-      fprintf(stderr, "Please provide the address of a file as an input.\n");
-      return -1;
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
 
     char sanitized_path[BUFSIZE];
     strncpy(sanitized_path, argv[1], BUFSIZE - 1);
     sanitized_path[BUFSIZE - 1] = '\0';
 
-    sanitize_path(sanitized_path);
-
     char cmd[BUFSIZE] = "wc -c < ";
-
-    // unsigned integer for objects size
     size_t buffer_left = BUFSIZE - strlen(cmd) - 1;
 
-    // Append sanitized user input to the command string
     strncat(cmd, sanitized_path, buffer_left);
     cmd[BUFSIZE - 1] = '\0';  // must end in null character
 
-    // copy to separate buffer
-    char command[BUFSIZE];
-    strncpy(command, cmd, BUFSIZE - 1);
-    command[BUFSIZE -1] = '\0';
+    // sanitize the final command
+    sanitize_path(cmd);
 
-    system(command);
+    system(cmd);
+    return 0;
 }
